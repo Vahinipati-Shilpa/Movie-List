19972557519b7acbb189c9759d447d3ed2a2e6a1
Yash Keshri <yash.keshri@syncron.com>
2019-11-25 20:27:20 +0530

    heroku deployment

--- src/app/app.util.ts
+++ src/app/app.util.ts
@@ -2,6 +2,6 @@
 export class AppUtil {
     constructor() {}
     static getUrl () {
-        return 'http://www.omdbapi.com/?apikey=2e2dc9e4';
+        return 'https://www.omdbapi.com/?apikey=2e2dc9e4';
     }
 }
